39d15212899983e95dc6a3387f085e3ef204b2d0
Gloria <gloria.palm7810@gmail.com>
2024-01-05 00:19:14 +0100

    Edit some files

--- backend/app.js
+++ backend/app.js
@@ -19,8 +19,8 @@ import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 const app = express();
 const httpServer = createServer(app); // Create the HTTP server instance
 
@@ -90,14 +90,14 @@ app.use((err, req, res, next) => {
 });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
