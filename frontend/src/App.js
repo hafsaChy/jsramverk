d9f89aec93a10348c1e14e9551e5b6cb42de0695
Gloria <gloria.palm7810@gmail.com>
2024-01-05 03:00:45 +0100

    Deploy frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { UserProvider } from "./contexts/usercontext";
 import Home from "./pages/Homepage";
 import Login from "./pages/Loginpage";
@@ -7,22 +7,22 @@ import Signup from "./pages/Signuppage";
  
 function App() {
  return (
-   <BrowserRouter>
+   <HashRouter basename="/" >
      {/* We are wrapping our whole app with UserProvider so that */}
      {/* our user is accessible through out the app from any page*/}
      <UserProvider>
        <Routes>
-         <Route exact path="/login" element={<Login />} />
-         <Route exact path="/signup" element={<Signup />} />
+         <Route path="/login" element={<Login />} />
+         <Route path="/signup" element={<Signup />} />
          {/* We are protecting our Home Page from unauthenticated */}
          {/* users by wrapping it with PrivateRoute here. */}
          <Route element={<PrivateRoute />}>
-           <Route exact path="/" element={<Home />} />
-           <Route exact path="/home" element={<Home />} />
+           <Route path="/" element={<Home />} />
+           <Route path="/home" element={<Home />} />
          </Route>
        </Routes>
      </UserProvider>
-   </BrowserRouter>
+   </HashRouter>
  );
 }
 
